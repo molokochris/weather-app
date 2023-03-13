e78d61e7bda56adaf6789c37ac9698ee2e3a8f70
molokochris <molokochrisp742@gmail.com>
2023-03-13 18:59:20 +0200

    fix search bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,44 @@
-const apiEndpoint = "http://api.openweathermap.org/data/2.5/weather?q=seshego&APPID=3ea1c8d09aeea877d65d2682296ce088";
+
 let locationEl = document.getElementById("location");
+let tempEl = document.getElementById("temperature");
+let searchValEl = document.getElementById("searchLoc");
 
+searchValEl.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    initApi();
+  }
+})
 
-fetch(apiEndpoint)
+function initApi () {
+
+  let input = searchValEl.value;
+  const apiEndpoint = `http://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=3ea1c8d09aeea877d65d2682296ce088`;
+
+  console.log(input);
+  
+  fetch(apiEndpoint)
   .then(response => response.json())
   .then(data => {
-    let temp = data.main.temp;
+    let temp = degree(data.main.temp);
     let country = data.sys.country;
     let city = data.name;
 
     console.log(data)
     console.log(data.sys.country);
-    console.log(degree(temp), country, city);
+    console.log(temp, country, city);
     locationEl.innerHTML = country + ", " + city;
+    tempEl.innerHTML = `<span>${temp}&deg;C</span>`;
   })
   .catch(error => {
     console.error(error);
   });
 
+}
+
+
 function degree(f) {
-  let c = (f - 32) / 1.8;
+  let c = f - 273.15;
   return Math.floor(c);
 }
 /* Set the width of the side navigation to 250px */
